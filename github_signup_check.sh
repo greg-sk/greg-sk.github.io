4253e55a1d445fd5298ec15c8ae481dd2f161413
greg-sk <grzegorz.luczywo@securitykiss.com>
2023-10-16 00:22:07 +0200

    work in progress

---
+++ github_signup_check.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+curl -s -XPOST --form "value=$1" --form "authenticity_token=ABv34vp4TszaUx4HJuOQsYnzVueSi8DJWj2nTjVAdGnAxJMYhMzozwDgrFZ5zY2t2Si8qcG9Y6qLKvUTMFd7XQ==" --cookie "logged_in=no; tz=Europe%2FWarsaw; color_mode=%7B%22color_mode%22%3A%22auto%22%2C%22light_theme%22%3A%7B%22name%22%3A%22light%22%2C%22color_mode%22%3A%22light%22%7D%2C%22dark_theme%22%3A%7B%22name%22%3A%22dark%22%2C%22color_mode%22%3A%22dark%22%7D%7D; _gh_sess=284PImVZhnKiF7jRp8PRBHZxVV7CaZNeVeZ61QtZhs5DGUWS%2FmMxYWm8rbkRX%2FK8WBAO8wqquY6qfWtuhI%2F3ke%2BwT85oxqh66%2BucOeNzY22TS%2FlZEzCsMVdaFzmWLsH%2FdQrIAVz81%2BXvgcj%2FLvHWfpdiAhGfn4oUURUtJsxP7b%2FEL0JXv0MqoJur32QiY4IefbvREfJa2noMoLD5cCMs5iu%2B44uA%2BokfhbUOEOxKiCvfIg0vc2bIZv0WeRdk%2FvHU9ir36z4BAP%2F1SPjWLh%2F9w2g0YN1AUwqNO01ukuQU7n90WA%2FLw4eTneG0qV%2B9fFnWZ8phc1notXZeUVN5g64jzGfYx7uL1V1Aopm2jJD3PoSiEtj1cZRjTX6DgLPa8Q%2BjuEFgsaL4116j32Ylu%2FAFZvKNBD17QolYx882baPokrIT46udKQ92CSKioqADl8%2BmVa3eImfZzOlAHuat0S%2FgUKVkZO8RenwQnBrX9XVpffC9NNYIpNyoDNHNMCskmVLDIFoBqnoKQqrehW73reFBlnxTBgZL5GLZg3OIl2X7jIB3H4fP--xBLOGPtK1yXw9QBD--zKkyqNUJlZk7rg3zSn8n%2Fw%3D%3D; preferred_color_mode=light; tz=Europe%2FWarsaw; _octo=GH1.1.215878890.1697400023"  https://github.com/signup_check/username  -A "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"; echo
+
+
+
+# for i in {2..99}; do ./github_signup_check.sh  pj"$i" | grep "is available" || echo -n .; done
+
+#.......pj32 is available.  #..pj35 is available.  #.......pj43 is available.  #............pj56 is available.  #..pj59 is available.  #..pj62 is available.  #pj63 is available.  #................pj80 is available.
+
+
+
+#.........xj11 is available.  #..............xj26 is available.  #.xj28 is available.  #..xj31 is available.  #........xj40 is available.  #..xj43 is available.  #...xj47 is available.  #............xj60 is available.  #..xj63 is available.  #.xj65 is available.  #.xj67 is available.  #..xj70 is available.  #xj71 is available.  #xj72 is available.  #xj73 is available.  #xj74 is available.  #.....xj80 is available.  #.xj82 is available.  #xj83 is available.  #...xj87 is available.  #......xj94 is available.
+
+
+#.........qx11 is available.  ..qx14 is available.  ......qx21 is available.  ..qx24 is available.  qx25 is available.  .qx27 is available.  qx28 is available.  qx29 is available.  ...qx33 is available.  qx34 is available.  qx35 is available.  qx36 is available.  .qx38 is available.  qx39 is available.  qx40 is available.  qx41 is available.  ....qx46 is available.  ..qx49 is available.  .qx51 is available.  qx52 is available.  qx53 is available.  .qx55 is available.  ..qx58 is available.  qx59 is available.  qx60 is available.  qx61 is available.  qx62 is available.  qx63 is available.  qx64 is available.  qx65 is available.  ....qx70 is available.  qx71 is available.  qx72 is available.  qx73 is available.  qx74 is available.  .qx76 is available.  .qx78 is available.  .qx80 is available.  qx81 is available.  qx82 is available.  qx83 is available.  qx84 is available.  .qx86 is available.  ..qx89 is available.  qx90 is available.  qx91 is available.  qx92 is available.  ...qx96 is available.
+
+
+
+#nostr2 is available.  nostr3 is available.  .nostr5 is available.  nostr6 is available.  nostr7 is available.  nostr8 is available.
+
+
+#...............ux17 is available.  ux18 is available.  ux19 is available.
+
+
+#................xp18 is available.
+
+
+#...............bj17 is available.
+
+
+#....ej40 is available.  .gj40 is available.  .ij40 is available.  ....nj40 is available.  ..qj40 is available.  ...uj40 is available.  vj40 is available.  wj40 is available.  xj40 is available.  .zj40 is available.
+
+#................qp40 is available.  .....wp40 is available.  .yp40 is available.  zp40 is available.
+
+#ax40 is available.  ..dx40 is available.  .fx40 is available.  .hx40 is available.  .jx40 is available.  kx40 is available.  ..nx40 is available.  ox40 is available.  .qx40 is available.  .sx40 is available.  tx40 is available.  ux40 is available.  vx40 is available.  wx40 is available.  xx40 is available.  yx40 is available.
+
+# domain fj32.org is available
+# domain bj17.org is available
+# domains: nostr2.me, nostr2.org, dj2.org
+
+
+
+
+
+
